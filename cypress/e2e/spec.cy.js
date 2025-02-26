987f71a37f18fdf89fcbbe6bfba207fc6515499e
Gerfson Alves <gerfson17@hotmail.com>
2025-02-26 15:39:41 -0300

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+//Ge@login - 1234
+
+describe('template spec', () => {
+  beforeEach('Login', () => {
+    cy.visit('https://barrigareact.wcaquino.me/login')
+  })
+})
