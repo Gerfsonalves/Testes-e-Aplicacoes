49d3d44d97e268d4ecd3afddf476d04b88bc99c3
Gerfson Alves <gerfson17@hotmail.com>
2025-02-26 16:04:40 -0300

    spec login

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,7 +3,15 @@
 //Ge@login - 1234
 
 describe('template spec', () => {
-  beforeEach('Login', () => {
-    cy.visit('https://barrigareact.wcaquino.me/login')
+  beforeEach('Homepage', () => {
+    cy.visit('https://barrigareact.wcaquino.me/')
   })
+
+  it('Login, email, senha e clique', () => {
+    cy.get('[data-test="email"]').type('Ge@login')
+    cy.get('[data-test="passwd"]').type('1234')
+    cy.get('.btn').click()
+    cy.get('.toast-message').should('contain', 'Bem vindo')
+  });
+
 })
