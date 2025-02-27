47d3f174f9b04851c265c7ccd38c333e0f86427e
Gerfson Alves <gerfson17@hotmail.com>
2025-02-27 14:48:35 -0300

    Criar conta

--- cypress/e2e/criateAccount.cy.js
+++ cypress/e2e/criateAccount.cy.js
@@ -4,12 +4,7 @@
 
 describe('template spec', () => {
   beforeEach('Homepage', () => {
-    cy.visit('https://barrigareact.wcaquino.me/')
-
-    cy.get('[data-test="email"]').type('Ge@login')
-    cy.get('[data-test="passwd"]').type('1234')
-    cy.get('.btn').click()
-    cy.get('.toast-message').should('contain', 'Bem vindo')
+    cy.login('Ge@login', '1234')
   })
 
   it('Inserir uma nova conta', () => {
@@ -17,7 +12,7 @@ describe('template spec', () => {
     cy.get('[href="/contas"]').click()
     cy.get('[data-test="nome"]').type('Conta nova cy')
     cy.get('.btn').click()
-    cy.get('.toast-message').should('contain', 'inserida com sucesso')
+
   })
 
 })
