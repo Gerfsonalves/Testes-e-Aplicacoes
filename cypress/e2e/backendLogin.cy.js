ad209f8626768bf8319d43707dcd52cae802966d
Gerfson Alves <gerfson17@hotmail.com>
2025-03-06 18:09:03 -0300

    Login

--- cypress/e2e/backendLogin.cy.js
+++ cypress/e2e/backendLogin.cy.js
@@ -1,15 +1,7 @@
 /// <reference types="cypress" />
 
 it('Login', () => {
-    cy.request({
-        method: 'POST',
-        url: 'https://barrigarest.wcaquino.me/signin',
-        body: {
-            email: "Ge@login",
-            redirecionar: false,
-            senha: "1234"
-        }
-    }).its('body.token').should('not.be.empty')
+    cy.getToken('Ge@login', '1234')
         .then(token => {
             cy.request({
                 url: 'https://barrigarest.wcaquino.me/contas',
